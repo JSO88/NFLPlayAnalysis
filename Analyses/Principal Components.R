b0b4b7fe41f94fd2012a4eea036ed44755103d12
JSO88 <j.sarinana.o@gmail.com>
2017-04-07 11:13:49 -0400

    Create Principal Components.R

---
+++ Analyses/Principal Components.R
@@ -0,0 +1,13 @@
+load("nflgames.sample.Rda")
+
+for (i in 1:ncol(nflgames.sample)){
+  if (class(nflgames.sample[,i])=='factor'){
+    nflgames.sample[,i] <- as.integer(nflgames.sample[,i])
+  }
+}
+
+nflgames.sample <- scale(nflgames.sample)
+
+pc_fit <- princomp(nflgames.sample, cor = TRUE)
+
+plot(pc_fit, type = "lines")
